f6aa0b7956b1f6a2779d363b55a3bced69146f53
Gingabread <35353121+Gingabread@users.noreply.github.com>
2024-10-16 16:16:40 -0600

    Modal Button Addition
    Added Modal button on main page

--- indexscripts.js
+++ indexscripts.js
@@ -69,3 +69,20 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Initial HTML overflow:', document.documentElement.style.overflow);
 });
 
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('openModal');
+const span = document.getElementsByClassName('close');
+
+btn.onclick = function () {
+    modal.style.display = 'block';
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
